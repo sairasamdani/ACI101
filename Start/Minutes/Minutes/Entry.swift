b139948577d893e64e8df2f5f9ee0a33bf5755f7
Saira Samdani <saira.samdani@volvocars.com>
2020-05-30 21:10:24 +0200

    Edited Entry file

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = "Hello World"
-    var content: String = ""
+    var title:   String = "Hello"
+    var content: String = "There"
     
     var id: String = UUID().uuidString
     
